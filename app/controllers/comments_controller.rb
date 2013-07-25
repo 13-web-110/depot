3a3d4e6ca8adf5b9b866f4b81381e4b072a206aa
JinnTao <314315805@qq.com>
2013-07-25 16:03:30 +0800

    add comment number to store page
    add comment number to store page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,20 @@ class CommentsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.create(params[:comment])
-    redirect_to product_path(@product)
+    @user    = User.find_by_id(session[:user_id])
+    @comment.commenter = @user.name
+    
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to product_path(@product) }
+        format.json { render json: @comment,
+          status: :created, location: @comment }
+      else
+        format.html { redirect_to product_path(@product), alert: ' Comment can not be nil. ' }
+      end
+    end
   end
+  
   def destroy
     @product = Product.find(params[:product_id])
     @comment = @product.comments.find(params[:id])
