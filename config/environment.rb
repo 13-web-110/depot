05d6c902289098288a636d73f8cfc5b355acb8ff
huangting <350358955@qq.com>
2013-07-30 17:16:15 +0800

    mailer and search

--- config/environment.rb
+++ config/environment.rb
@@ -6,11 +6,12 @@ Depot::Application.initialize!
 
 
 Depot::Application.configure do
+config.mailer_sender = "13web110@gmail.com"
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
 port: 587,
-# domain: "domain.of.sender.net",
+domain: "heroku.com",
 authentication: "plain",
 # user_name: "dave",
 user_name: "13web110@gmail.com",
