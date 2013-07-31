7f5d03cd6d4cd0674600cf8b2a350536dda1cc5a
huangting <350358955@qq.com>
2013-07-31 16:11:13 +0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,13 @@
+def compile_asset?(path)
+  if File.basename(path) =~ /^[^_].*\.\w+$/
+    puts "Compiling: #{path}"
+    true
+  else
+    puts "Ignoring: #{path}"
+    false
+  end
+end
+
 Depot::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   config.action_mailer.delivery_method = :smtp
@@ -57,18 +67,9 @@ Depot::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += [ Proc.new {|path| File.basename(path) =~ /^[^_].*\.\w+$/} ]
+  # config.assets.precompile += [ Proc.new {|path| File.basename(path) =~ /^[^_].*\.\w+$/} ]
   # config.assets.precompile += %w[js css].map {|ext| Dir[Rails.root.join("app/assets/*/*.#{ext}")] }.flatten.map {|f| f.split('/')[-1] }
-  # def compile_asset?(path)
-  #   if File.basename(path) =~ /^[^_].*\.\w+$/
-  #     puts "Compiling: #{path}"
-  #     true
-  #   else
-  #     puts "Ignoring: #{path}"
-  #     false
-  #   end
-  # end
-  # config.assets.precompile = [ method(:compile_asset?).to_proc ]
+  config.assets.precompile = [ method(:compile_asset?).to_proc ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
