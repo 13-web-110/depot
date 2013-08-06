61adc17958d18dd7e8acd556e8ab7e650fd73708
huangting <350358955@qq.com>
2013-08-06 13:12:53 +0800

    category file uploader

--- app/models/root_category.rb
+++ app/models/root_category.rb
@@ -1,3 +1,8 @@
 class RootCategory < ActiveRecord::Base
   attr_accessible :description, :image_url, :title
+  validates :title, :description, :image_url, presence: true
+  validates :image_url, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
